ef5c3905bc50cc87d4acd441ffd7ed53010c4ae1
johnarvi <arvind.john@mail.utoronto.ca>
2019-10-23 17:49:00 +0000

    updated the movements- still hard coded

--- client.js
+++ client.js
@@ -6,8 +6,17 @@ const connect = function() {
   });
 
   conn.setEncoding('utf8');
-  conn.on('connect', () => console.log("Successfully connected to game server"));
-  conn.on('connect', () => conn.write("Name: AJJ"));
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AJJ");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+    // setInterval(() => {
+    //   conn.write("Move: right");
+    // }, 100);
+  });
+
   conn.on('data', (data) => console.log("server says: ", data));
 
   return conn;
